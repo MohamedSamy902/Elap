81afc36f30daf5ed430d654110fd752c078044b3
baraaofficial <baraa_official@outlook.com>
2022-02-16 16:56:16 +0200

    some upsate*

---
+++ database/migrations/2022_02_16_143646_create_history_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoryProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('history_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('serial_number');
+            $table->string('status')->default(0);
+            $table->integer('user_id');
+            $table->integer('product_id');
+            $table->timestamps();
+            $table->timestamp('end_at')->default(null);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('history_products');
+    }
+}
---
+++ database/migrations/2022_02_16_143828_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_product');
+            $table->string('damage');
+            $table->integer('count')->default(1);
+            $table->string('product_inclusions')->default('لا يوجد');
+            $table->string('serial_number');
+            $table->integer('category_id');
+            $table->integer('customers_id');
+            $table->integer('user_id')->nullable();
+            $table->string('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
